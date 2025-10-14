7a9ca65318f0714ab1a47187c1c33db81a6b26e7
SadDisplay <1212236@student.roc-nijmegen.nl>
2025-10-14 13:40:26 +0200

    Heb een foto en header toegevoegd aan de Index pagina

--- html/index.php
+++ html/index.php
@@ -7,12 +7,20 @@
 <head>
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Darko's</title>
 </head>
-<body>
-<p>Test</p>
+<main>
+    <body>
+    <header>
+        <div class="container-header">
+            <img src="images/logo.png" alt="Logo Darko's">
+            <a href="">Login</a>
+            <a href="">Home</a>
+        </div>
+    </header>
+    <p>Test</p>
+</main>
 </body>
 </html>
