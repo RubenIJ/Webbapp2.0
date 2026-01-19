777003eeac5faa7af3f4283f87c5807ee983020a
SadDisplay <1212236@student.roc-nijmegen.nl>
2026-01-19 10:35:35 +0100

    Kan nu via add.php dingen toevoegen in de database met een INSERT INTO SQL statement

---
+++ html/add.php
@@ -0,0 +1,49 @@
+<?php
+ob_start();
+session_start();
+
+if (!isset($_SESSION['adminjanee']) or $_SESSION['adminjanee'] == false) {
+    header('Location: login.php');
+}
+
+
+if (isset($_POST['naam'], $_POST['omschrijving'], $_POST['prijs'])) {
+
+    $naam = $_POST['naam'];
+    $omschrijving = $_POST['omschrijving'];
+    $prijs = $_POST['prijs'];
+
+    $stmt = $pdo->prepare("INSERT INTO menu (naam, omschrijving, prijs) VALUES (:naam, :omschrijving, :prijs)");
+
+    $stmt->bindParam(':naam', $naam);
+    $stmt->bindParam(':omschrijving', $omschrijving);
+    $stmt->bindParam(':prijs', $prijs);
+
+    $stmt->execute();
+
+    header('Location: admin.php');
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Toevoegen</title>
+    <?php
+    include_once 'header.php';
+    ?>
+</head>
+<body>
+<form method="post" action="add.php">
+    <input name="naam" type="text" placeholder="Naam">
+    <input name="omschrijving" type="text" placeholder="Omschrijving">
+    <input name="prijs" type="text" placeholder="Prijs">
+    <button type="submit">Toevoegen</button>
+</form>
+</body>
+</html>
--- html/admin.php
+++ html/admin.php
@@ -12,7 +12,7 @@ session_start();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Admin</title>
     <?php
     include_once 'header.php';
 
@@ -22,6 +22,23 @@ session_start();
     ?>
 </head>
 <body>
+<a href="add.php" >Product toevoegen</a>
+ <h2>menu kaart</h2>
+    <?php
+    $sql = "SELECT id, naam, omschrijving, prijs FROM menu";
+    $stmt = $pdo->query($sql);
+
+    $rows = $stmt->fetchAll(); // Haalt alle data uit de database en zet het in de variable rows
+
+    if (count($rows) > 0) { // count telt hoeveel rijen er in de database staan het vergelijkt het met het getal 0, als het meer dan 0 is gaat de foreach loop lopen
+        foreach ($rows as $row) {
+            echo "" . $row["naam"] . " - " . $row["omschrijving"] . " - " . $row["prijs"]  . "€" . "<br>"; //dit laat alle rijen zien met de data: naam, omschrijving en prijs
+        }
+    } else {
+        echo "0 results";
+    }
+    ?>
+
 
 </body>
 </html>
